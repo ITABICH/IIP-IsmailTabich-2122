fa7a39bab5c5f153dca20f60475c9dadaf03901d
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-11-21 06:44:11 +0100

    01.IIP_VisualStudioEnWpf

---
+++ 01.IIP-VisualStudioEnWpf/01.IIP-VisualStudioEnWpf/wpfPizza/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rbtn_Spicy_Checked(object sender, RoutedEventArgs e)
+        {
+            TxtBlk_Ingredienten.Text = "Tomatensaus, mozzarella, ui, kip, paprika, pepperoni & spaanse pepers";
+            imgPizza.Source = new BitmapImage(new Uri("Photos/HotAndSpicy.jpg", UriKind.Relative));
+        }
+
+        private void Rbtn_Tonno_Checked(object sender, RoutedEventArgs e)
+        {
+            TxtBlk_Ingredienten.Text = "Tomatensaus, mozzarella, tonijn, rode ui, zwarte olijven & verse spinazie";
+            imgPizza.Source = new BitmapImage(new Uri("Photos/Tonno.jpg", UriKind.Relative));
+
+        }
+
+        private void Rbtn_BbqGrill_Checked(object sender, RoutedEventArgs e)
+        {
+            TxtBlk_Ingredienten.Text = "BBQ saus, mozzarella, ui, gehakt, bacon & een swirl heerlijke BBQ saus";
+            imgPizza.Source = new BitmapImage(new Uri("Photos/Tonno.jpg", UriKind.Relative));
+
+        }
+    }
+}
