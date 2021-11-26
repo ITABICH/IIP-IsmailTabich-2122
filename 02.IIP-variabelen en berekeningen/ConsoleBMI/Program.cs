545984a9c5127fd5773e8186bc434d508d7bfb1f
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-11-26 12:38:21 +0100

    Opdrachten klaar

--- 02.IIP-variabelen en berekeningen/ConsoleBMI/Program.cs
+++ 02.IIP-variabelen en berekeningen/ConsoleBMI/Program.cs
@@ -1,23 +1,30 @@
 ﻿class BMI
 {
+
+
     static void Main(string[] args)
     {
-        
-
         Console.WriteLine("BMI CALCULATOR");
         Console.WriteLine("==============");
-        Console.ReadLine();
 
-        double Lengte;
-        double Gewicht;
+        //Lengte tijpen
+        Console.Write("Lengte (in cm): ");
+        int lengteCm = Convert.ToInt32(Console.ReadLine());
+        //Voor dat ik de Youtube Video heb gezien ==> string lengteCm = Console.ReadLine();
 
-        Console.Write($"Lengte (in cm): ");
-        Lengte = Console.Read();
-        Console.ReadLine();
-        Console.Write($"Gewicht (in kg): ");
-        Gewicht = Console.Read();
-        double BMI;
-        BMI= Gewicht / ((Lengte /100) * 2);
-        Console.Write($"Je BMI bedraagt: " + (BMI));
+        //Gewicht tijpen
+        Console.Write("Gewicht (in kg):");
+        int gewicht = Convert.ToInt32(Console.ReadLine());
+        // Voor dat ik de Youtube Video heb gezien ==> string gewicht = Console.ReadLine();
+
+        //Lengte van centimeter naar meter wisselen
+        double lengteM = Convert.ToDouble(lengteCm) / 100;  
+        
+
+        //Berekening van het BMI
+        double BMI = Convert.ToDouble(gewicht) / (lengteM * lengteM);
+
+        //Het resultaat van de BMI berekening
+        Console.Write($"Je BMI bedraagt: " + (BMI = Math.Round(BMI, 2)));
     }
 }
---
+++ 02.IIP-variabelen en berekeningen/ConsoleKarakter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+class Karakter
+{
+    static void Main(string[] args)
+    {
+
+        //Geef een letter in
+        Console.Write("Geef een kleine letter: ");
+        char letter = Convert.ToChar(Console.ReadLine());
+
+        //Het letternummer van de vorige letter uit de ASCII
+        Console.WriteLine($"Het nummer is " + (byte)letter);
+
+        //De hoofdletter aantonon
+        Console.WriteLine("De hoofdletter is " + (char)Convert.ToChar(letter - 32));
+
+        //De vorige letter van de initiale letter
+        Console.WriteLine("De vorige letter is " + (char)Convert.ToChar(letter ++));
+
+        //De volgende letter van de initiale letter
+        Console.WriteLine("De volgende letter is " + (char)Convert.ToChar(letter--));
+    }
+}
