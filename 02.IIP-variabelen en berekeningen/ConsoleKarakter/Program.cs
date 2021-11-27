c2597bc779be5166bb08317f4a27552ff268d894
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-11-27 02:29:03 +0100

    Enkele foutjes verbeterd

--- 02.IIP-variabelen en berekeningen/ConsoleKarakter/Program.cs
+++ 02.IIP-variabelen en berekeningen/ConsoleKarakter/Program.cs
@@ -16,9 +16,9 @@ class Karakter
         Console.WriteLine("De hoofdletter is " + (char)Convert.ToChar(letter - 32));
 
         //De vorige letter van de initiale letter
-        Console.WriteLine("De vorige letter is " + (char)Convert.ToChar(letter ++));
+        Console.WriteLine("De vorige letter is " + (char)Convert.ToChar(letter+1));
 
         //De volgende letter van de initiale letter
-        Console.WriteLine("De volgende letter is " + (char)Convert.ToChar(letter--));
+        Console.WriteLine("De volgende letter is " + (char)Convert.ToChar(letter-1));
     }
 }
