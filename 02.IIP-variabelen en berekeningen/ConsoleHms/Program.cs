0b66545cabc03974063cd10900a778733e8e27b5
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-11-29 15:29:55 +0100

    NIET KLAAT

---
+++ 02.IIP-variabelen en berekeningen/ConsoleHms/Program.cs
@@ -0,0 +1,28 @@
+﻿class Hms
+{
+    static void Main(string[] args)
+    {
+        //Aantal seconden invoegen
+        Console.Write("Geef het aantal seconden: ");
+        int aantalSeconden = Convert.ToInt32(Console.ReadLine());
+
+        //Berekening in uren
+        int uren = aantalSeconden / 3600;
+
+        //Rest van de berekening in uren
+        int restUren = aantalSeconden % 3600;
+
+        //Berekening in minuten
+        int minuten = restUren / 60;
+
+        //Rest van de berekening in minuten
+        int restMinuten = restUren % 60;
+
+        //Berekening in seconden
+        int seconden = restMinuten / 1; 
+
+        //Het omzet van seconden naar uren minuten en seconden
+        Console.Write($"Omgezet in hms formaat: {uren}:{minuten}:{seconden} ");
+
+    }
+}
---
+++ 02.IIP-variabelen en berekeningen/WpfWinkel/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWinkel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const double PRIJS_BANAAN = 2.25;
+        const double PRIJS_BOTER = 3.05;
+        const double PRIJS_EIEREN = 2.95;
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Random voorraad = new Random();
+
+            
+            lblVoorraadBanaan.Content = voorraad.Next(5, 40);
+            
+            lblVoorraadBoter.Content = voorraad.Next(5, 40);
+            
+            lblVoorraadEieren.Content = voorraad.Next(5, 40);
+
+
+
+
+        }
+
+        private void btnBanaan_Click(object sender, RoutedEventArgs e)
+        {
+            int voorraadBanaan = Convert.ToInt32(lblVoorraadBanaan.Content)
+            lblVoorraadBanaan.Content = voorraadBanaan - 1;
+            lstBxVerkocht.Items.Add($"{PRIJS_BANAAN} - 1 tros banaan");
+
+        }
+
+        private void btnBoter_Click(object sender, RoutedEventArgs e)
+        {
+            int voorraadBoter = Convert.ToInt32(lblVoorraadBanaan.Content);
+            lblVoorraadBoter.Content = voorraadBoter - 1;
+            lstBxVerkocht.Items.Add($"{PRIJS_BOTER} - 1 pak boter");
+
+        }
+
+        private void btnEieren_Click(object sender, RoutedEventArgs e)
+        {
+            int voorraadEieren = Convert.ToInt32(lblVoorraadEieren.Content);
+            lblVoorraadEieren.Content = voorraadEieren - 1;
+            lstBxVerkocht.Items.Add($"{PRIJS_EIEREN} - 1 pak boter");
+        }
+
+        private void btnHerstel_Click(object sender, RoutedEventArgs e)
+        {
+            lstBxVerkocht.Items.Clear();
+            lblVoorraadBanaan.Content = "";
+            lblVoorraadBoter.Content = "";
+            lblVoorraadEieren.Content = "";
+            lblAfrekenen.Content = "";
+
+            Random voorraad = new Random();
+
+            lblVoorraadBanaan.Content = voorraad.Next(5, 40);
+            lblVoorraadBoter.Content = voorraad.Next(5, 40);
+            lblVoorraadEieren.Content = voorraad.Next(5, 40);
+        }
+
+        private void btnAfrekenen_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+
+        }
+    }
+}
