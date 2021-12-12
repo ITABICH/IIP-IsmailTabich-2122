972a975ab42d7c898e500c8c434ed5c2f90bb30b
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-12-12 16:53:40 +0100

    Event & selecties

--- 03.B.IIP-events/WpfGebruikersNaam/MainWindow.xaml.cs
+++ 03.B.IIP-events/WpfGebruikersNaam/MainWindow.xaml.cs
@@ -27,21 +27,36 @@ namespace WpfGebruikersNaam
 
         private void txtBxGebruikersnaam_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Space)
+            if (txtBxGebruikersnaam.Text.Contains(" ") || txtBxGebruikersnaam.Text.Contains("@") ||
+                  txtBxGebruikersnaam.Text.Contains("_") || txtBxGebruikersnaam.Text.Contains(".") ||
+                  txtBxGebruikersnaam.Text.Contains("-") || txtBxGebruikersnaam.Text.Contains("1") ||
+                  txtBxGebruikersnaam.Text.Contains("2") || txtBxGebruikersnaam.Text.Contains("3") ||
+                  txtBxGebruikersnaam.Text.Contains("4") || txtBxGebruikersnaam.Text.Contains("5") ||
+                  txtBxGebruikersnaam.Text.Contains("6") || txtBxGebruikersnaam.Text.Contains("7") ||
+                  txtBxGebruikersnaam.Text.Contains("8") || txtBxGebruikersnaam.Text.Contains("9") ||
+                  txtBxGebruikersnaam.Text.Contains("0"))
             {
                 txtBxGebruikersnaam.Background = Brushes.Red;
-                lblFoutmelding.Content = "Mag geen spaties bevatten";
+                lblFoutmelding.Content = "Mag geen spaties bevatten, cijfers of symbolen bevatten";
                 lblFoutmelding.Foreground = Brushes.Red;
             }
 
-            else if (e.Key == Key.NumPad0 || e.Key == Key.NumPad1 || e.Key == Key.NumPad2 ||
-                e.Key == Key.NumPad3 || e.Key == Key.NumPad4 || e.Key == Key.NumPad5 ||
-                e.Key == Key.NumPad6 || e.Key == Key.NumPad7 || e.Key == Key.NumPad8 || e.Key == Key.NumPad9)
-            {
-                txtBxGebruikersnaam.Background = Brushes.Red;
-                lblFoutmelding.Content = "Mag geen cijfers bevatten";
-                lblFoutmelding.Foreground= Brushes.Red; 
-            }
+            ///if (e.Key == Key.Space)
+            ///{
+            ///txtBxGebruikersnaam.Background = Brushes.Red;
+            ///lblFoutmelding.Content = "Mag geen spaties bevatten";
+            ///lblFoutmelding.Foreground = Brushes.Red;
+            ///}
+
+            ///else if (e.Key == Key.NumPad0 || e.Key == Key.NumPad1 || e.Key == Key.NumPad2 ||
+            ///e.Key == Key.NumPad3 || e.Key == Key.NumPad4 || e.Key == Key.NumPad5 ||
+            ///e.Key == Key.NumPad6 || e.Key == Key.NumPad7 || e.Key == Key.NumPad8 || e.Key == Key.NumPad9)
+            ///{
+            ///txtBxGebruikersnaam.Background = Brushes.Red;
+            ///lblFoutmelding.Content = "Mag geen cijfers bevatten";
+            ///lblFoutmelding.Foreground= Brushes.Red; 
+            ///}
+
 
             else if (txtBxGebruikersnaam.Text == "")
             {
@@ -53,6 +68,21 @@ namespace WpfGebruikersNaam
                 txtBxGebruikersnaam.Background= Brushes.Green;
                 lblFoutmelding.Content = "";
             }
+
+            if(txtBxGebruikersnaam.Text.Contains (" ") || txtBxGebruikersnaam.Text.Contains("@") || 
+               txtBxGebruikersnaam.Text.Contains ("_") || txtBxGebruikersnaam.Text.Contains(".") ||
+               txtBxGebruikersnaam.Text.Contains("-") || txtBxGebruikersnaam.Text.Contains("1") || 
+               txtBxGebruikersnaam.Text.Contains("2") || txtBxGebruikersnaam.Text.Contains("3") ||
+               txtBxGebruikersnaam.Text.Contains("4") || txtBxGebruikersnaam.Text.Contains("5") ||
+               txtBxGebruikersnaam.Text.Contains("6") || txtBxGebruikersnaam.Text.Contains("7") ||
+               txtBxGebruikersnaam.Text.Contains("8") || txtBxGebruikersnaam.Text.Contains("9") ||
+               txtBxGebruikersnaam.Text.Contains("0"))
+            {
+                txtBxGebruikersnaam.Background = Brushes.Red;
+                lblFoutmelding.Content = "Mag geen spaties bevatten, cijfers of symbolen bevatten";
+                lblFoutmelding.Foreground = Brushes.Red;
+            }
+
         }
     }
 }
