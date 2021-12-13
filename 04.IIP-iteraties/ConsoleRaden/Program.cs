32ee2ae893f1731d46e30fd8002c4e98ad817d2c
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-12-13 15:05:44 +0100

    commit

---
+++ 04.IIP-iteraties/ConsoleRaden/Program.cs
@@ -0,0 +1,28 @@
+﻿class Raden
+{
+    static void Main(String[] args)
+    {
+        int geheimGetal = 3;
+        Console.WriteLine("Raad een getal tussen 0 en 10");
+
+
+        int poging = 0;
+        int getal;
+       
+        do
+        {
+            poging++;
+            if (poging > 3)
+            {
+                Console.WriteLine("Volgende keer beter");
+            }
+            else
+            Console.Write($"Poging {poging}: ");
+            getal = Convert.ToInt32(Console.ReadLine());
+            
+            
+        }
+        while (getal != geheimGetal);
+        Console.WriteLine("Geraden!");
+    }
+}
