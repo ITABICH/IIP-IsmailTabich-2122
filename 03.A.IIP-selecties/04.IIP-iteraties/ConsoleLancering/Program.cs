0b0909da316d1577ce2b118ac16c779504874e72
Ismail Tabich <ismail.tabich@student.odisee.be>
2021-12-13 12:47:22 +0100

    commit

---
+++ 03.A.IIP-selecties/04.IIP-iteraties/ConsoleLancering/Program.cs
@@ -0,0 +1,18 @@
+﻿class Lancering
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hoeveel seconden tot lancering? ");
+        int seconden = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = seconden; i > 0; i--)
+        {
+            seconden = i;
+            Console.Write($"{seconden} ...");
+            Console.ReadLine();
+        }
+
+        Console.WriteLine("Lift off!");
+
+    }
+}
